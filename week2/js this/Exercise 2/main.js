68be40d718cbd4cd9114758b866a10de8aa33e81
Dmitry Sokolov <urbi@varenye.com>
2023-06-08 18:38:36 +0300

    add second exercise js this

---
+++ week2/js this/Exercise 2/main.js
@@ -0,0 +1,23 @@
+const pump = function (amount) {
+  this.liters += amount;
+  console.log('You put ' + amount + ' liters in ' + this.name);
+};
+
+const garage = {
+  car1: {
+    name: 'Audi',
+    liters: 3,
+    fillTank: pump
+  },
+  car2: {
+    name: 'Mercedes',
+    liters: 1,
+    fillTank: pump
+  }
+};
+
+garage.car1.fillTank(2);
+console.log('Audi should have 5 liters: ',  garage.car1.liters);
+
+garage.car2.fillTank(30);
+console.log('Mercedes should have 31 liters: ', garage.car2.liters);
